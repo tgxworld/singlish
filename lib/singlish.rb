ba468aa6733eb5310df0fee74a4c11ceaa0fde37
Guo Xiang Tan <tgx_world@hotmail.com>
2015-05-08 17:56:24 +0800

    'loh' -> 'lor'.

--- lib/singlish.rb
+++ lib/singlish.rb
@@ -1,6 +1,6 @@
 class Object
   def method_missing(method_name, *args, &block)
-    if(method_name =~ /(.*)_(lah|loh|leh)/)
+    if(method_name =~ /(.*)_(lah|lor|leh)/)
       send($1, *args, &block)
     end
   end
